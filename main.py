9e7d1d83652f69bdef279c599a1a226199fa7f89
Aldrich Agabin <arcflu52@gmail.com>
2022-08-20 03:53:28 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+from asyncore import loop
+from random import random
+import random
+from re import search
+
+def initBoxes() -> list[int]:
+    boxes = [0] * numPrisoners
+    chosenDict = {}
+    currIndex = 0
+    while len(chosenDict) < numPrisoners:
+        number = random.choice(range(0, numPrisoners))
+        if number not in chosenDict:
+            chosenDict[number] = currIndex
+            boxes[currIndex] = number
+            currIndex += 1
+
+    # for b in range(0, len(boxes), 10):
+    #     print(boxes[b:b+10])
+
+    return boxes
+
+def searchBoxes(boxes, prisonerNumber) -> int:
+    currBox = boxes[prisonerNumber]
+    loopLength = 1
+    while currBox != prisonerNumber:
+        if loopFlag:
+            if loopLength > maxLoopLength:
+                return loopLength
+
+        currBox = boxes[currBox]
+        loopLength += 1
+    
+    return loopLength
+
+def simulation() -> bool:
+    boxes = initBoxes()
+    maxLoop = 0
+    for i in range(0, numPrisoners):
+        loopLength = searchBoxes(boxes, i)
+        if loopFlag and loopLength > maxLoopLength:
+            return False
+        maxLoop = max(maxLoop, loopLength)
+
+    sumLoops.append(maxLoop)
+    if maxLoop > maxLoopLength:
+        return False
+
+    return True
+
+numPrisoners = 100
+maxLoopLength = 50
+sumLoops = []
+loopFlag = True
+failures = 0
+successes = 0
+numCases = 1000
+for i in range(0, numCases):
+    if simulation():
+        successes += 1
+    else:
+        failures += 1
+
+successRate = float(successes/numCases)
+
+print()
+print('Success Rate: ' + f'{successRate:.3f}')
+if loopFlag:
+    print('Average size of successful loops: ' + str(sum(sumLoops)/successes))
+else:
+    print('Average size of all loops: ' + str(sum(sumLoops)/numCases))
+print()
